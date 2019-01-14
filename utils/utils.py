5d60e924aab35e85f2204992913462f19b19aceb
FienSoP <sahir.sofiane@gmail.com>
2019-01-14 15:30:22 +0100

    Final version

---
+++ utils/utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+import skimage
+import matplotlib.pyplot as plt 
+import matplotlib.image as mpimg
+import os
+import scipy.misc as sm
+
+def rgb2gray(rgb):
+
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+    return gray
+
+def load_data(dir_name = 'faces_imgs'):    
+    '''
+    Load images from the "faces_imgs" directory
+    Images are in JPG and we convert it to gray scale images
+    '''
+    imgs = []
+    for filename in os.listdir(dir_name):
+        img = mpimg.imread(dir_name + '/' + filename)
+        img = rgb2gray(img)
+        imgs.append(img)
+    return imgs
+
+
+def visualize(imgs, format=None):
+    plt.figure(figsize=(20, 40))
+    for i, img in enumerate(imgs):
+        if img.shape[0] == 3:
+            img = img.transpose(1,2,0)
+        plt_idx = i+1
+        plt.subplot(4, 2, plt_idx)    
+        plt.imshow(img, format)
+    plt.show()
+
+    
